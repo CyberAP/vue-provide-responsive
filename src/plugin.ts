d3334b56017408165785e021585ec8b03f3746d9
Stas Lashmanov <stasvarenkin@gmail.com>
2019-12-28 00:48:03 +0300

    TS fixes

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,6 +3,7 @@ const getWindow = () => typeof window === 'undefined' ? null : window;
 type Responsive = { width: number } & Record<string, any>;
 type Breakpoint = number | { value: number, ignoreScrollbar: boolean };
 type Breakpoints = Record<string, Breakpoint>;
+type ResponsiveHelper = (this: Responsive) => boolean;
 
 const install = (Vue: any, {
   breakpoints = {},
@@ -22,24 +23,24 @@ const install = (Vue: any, {
 
   const scrollbarWidth = (() => {
     if (!window) return 0;
-    return window.innerWidth - window.document.documentElement.clientWidth;
+    return window.innerWidth - window.document.body.clientWidth;
   })();
 
   const helpers = Object.keys(breakpoints)
     .reduce((acc, key) => {
       const breakpoint = breakpoints[key];
       const name = 'is' + key.charAt(0).toUpperCase() + key.substring(1); // mobile → isMobile
-      let value, ignoreScrollbar = false;
+      let value: number, ignoreScrollbar = false;
       if (typeof breakpoint === 'object') {
         value = breakpoint.value;
         ignoreScrollbar = breakpoint.ignoreScrollbar;
       } else {
         value = breakpoint;
       }
-      const withScrollbar = function responsiveHelper() {
+      const withScrollbar: ResponsiveHelper = function responsiveHelper() {
         return this.width + scrollbarWidth <= value;
       };
-      const withoutScrollbar = function responsiveHelper() {
+      const withoutScrollbar: ResponsiveHelper = function responsiveHelper() {
         return this.width <= value;
       };
       Object.defineProperty(acc, name, {
