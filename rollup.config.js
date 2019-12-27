b7d10963a9b21c8180fcc656b3a1f2afa53df9d9
Stas Lashmanov <stasvarenkin@gmail.com>
2019-12-28 01:29:45 +0300

    Updated build scenario

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
 export default {
@@ -16,5 +17,8 @@ export default {
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
-  ], plugins: [typescript()],
+  ], plugins: [
+    terser(),
+    typescript()
+  ],
 }
