c45afe0be0273d5ac0f6b2c518f8d3410514a3bf
Stas Lashmanov <stasvarenkin@gmail.com>
2019-12-28 00:11:23 +0300

    Api change

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,8 @@
 const getWindow = () => typeof window === 'undefined' ? null : window;
 
 type Responsive = { width: number } & Record<string, any>;
-type Breakpoints = Record<string, number>;
+type Breakpoint = number | { value: number, ignoreScrollbar: boolean };
+type Breakpoints = Record<string, Breakpoint>;
 
 const install = (Vue: any, {
   breakpoints = {},
@@ -9,31 +10,40 @@ const install = (Vue: any, {
   window = getWindow(),
   onResize,
   name = 'responsive',
-  respectScrollbarWidth = true,
 }: {
   breakpoints?: Breakpoints,
   defaultWidth?: number,
   window?: Window | null,
   onResize?: (event: UIEvent, responsive: Responsive) => void,
   name?: string | symbol,
-  respectScrollbarWidth?: boolean,
 } = {}) => {
 
   const scrollbarWidth = (() => {
-    if (!window || !respectScrollbarWidth) return 0;
+    if (!window) return 0;
     return window.innerWidth - window.document.documentElement.clientWidth;
   })();
 
   const helpers = Object.keys(breakpoints)
     .reduce((acc, key) => {
-      const value = breakpoints[key];
+      const breakpoint = breakpoints[key];
       const name = 'is' + key.charAt(0).toUpperCase() + key.substring(1); // mobile → isMobile
+      let value, ignoreScrollbar = false;
+      if (typeof breakpoint === 'object') {
+        value = breakpoint.value;
+        ignoreScrollbar = breakpoint.ignoreScrollbar;
+      } else {
+        value = breakpoint;
+      }
+      const withScrollbar = function responsiveHelper() {
+        return this.width + scrollbarWidth <= value;
+      };
+      const withoutScrollbar = function responsiveHelper() {
+        return this.width <= value;
+      };
       Object.defineProperty(acc, name, {
         configurable: true,
         enumerable: true,
-        get() {
-          return this.width + scrollbarWidth <= value;
-        },
+        get: ignoreScrollbar ? withoutScrollbar : withScrollbar,
       });
       return acc;
     }, {});
